9df1db3827d95c7f608e2101e5aa0de02dbd19ee
sadakpramodh <sadakpramodh@outlook.com>
2025-06-23 22:06:17 +0530

    added google-services.json to gitignore

--- auth.py
+++ auth.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MAIL_SERVER = os.getenv("MAIL_SERVER")
-MAIL_PORT = os.getenv("MAIL_PORT")
+MAIL_PORT = int(os.getenv("MAIL_PORT", 587))  # Default to port 587 if not specified
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
